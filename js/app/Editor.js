e9860b7cd2289bc084f43278f2ed892a68b1f668
Orsós Lívia <orsoslivia@hotmail.com>
2021-05-26 22:47:51 +0200

    elemek listája és törlése

--- js/app/Editor.js
+++ js/app/Editor.js
@@ -25,6 +25,7 @@ class Editor {
                     const selectSize = document.getElementById('selectSize');
                     const coloringEditor = document.getElementById('itemColor');
                     const addBtn = document.getElementById('addBlock');
+                    const printExistingBlocks = document.getElementById('blockList');
                     if(selectSize){
                         selectSize.addEventListener('change', () => {
                             this.matrix = [];
@@ -40,6 +41,9 @@ class Editor {
                         addBtn.addEventListener('click', () => {
                             this.addBlock();
                         });
+                    }
+                    if(printExistingBlocks){
+                        this.printExistingBlocks();
                     }
                 }
             }.bind(this));
@@ -57,13 +61,67 @@ class Editor {
             this.player.start();
         }
     }
+    deleteObject(object){
+        const customObjects = localStorage.getItem('objects') ? JSON.parse(localStorage.getItem('objects')) : [];
+        const result = customObjects.filter(obj => JSON.stringify(obj) != JSON.stringify(object));
+        localStorage.setItem('objects', JSON.stringify(result));
+        this.printExistingBlocks();
+    }
+    getSquare(isFilled, color){
+        let square = document.createElement('div');
+        if(isFilled == 0){
+            square.classList.add('small-square');
+        }else{
+            square.classList.add('small-square');
+            square.style.backgroundColor = color;
+        }
+        return square;
+    }
+    getLine(line, color){
+        let squareLine = document.createElement('div');
+        squareLine.classList.add('square-line');
+        for(let i = 0; i < line.length; i++){
+            squareLine.appendChild(this.getSquare(line[i], color));
+        }
+        return squareLine;
+    }
+    getBlock(object, deleteable){
+        let block = document.createElement('div');
+        block.classList.add('block');block.classList.add('d-flex');block.classList.add('align-center');block.classList.add('justify-content-center');block.classList.add('col-auto');
+        if(deleteable){
+            let emoji = document.createElement('div');
+            emoji.classList.add('delete-emoji');
+            emoji.innerText = '❌';
+            emoji.addEventListener('click', () => this.deleteObject(object));
+            block.appendChild(emoji);
+        }
+        let container = document.createElement('div');
+        for(let i = 0; i < object.matrix.length; i++){
+            container.appendChild(this.getLine(object.matrix[i], object.color));
+        }
+        block.appendChild(container);
+        return block;
+    }
+    printExistingBlocks(){
+        const e = document.getElementById('blockList');
+        if(e){
+            emptyElement(e);
+            const objects = localStorage.getItem('objects') ? JSON.parse(localStorage.getItem('objects')) : [];
+            for(let i = objects.length-1; i >= 0; i--){
+                e.appendChild(this.getBlock(objects[i], true));
+            }
+            for(let i = 0; i < Objects.length; i++){
+                e.appendChild(this.getBlock(Objects[i], false));
+            }
+        }
+    }
     printEditor(){
-        const e = document.getElementById("selectSize");
+        const e = document.getElementById('selectSize');
         const size = e.value;
         this.renderBox(size);
     }
     renderBox(size){
-        const e = document.getElementById("shapeEditor");
+        const e = document.getElementById('shapeEditor');
         if(e){
             emptyElement(e);
             for(let i = 0; i < size; i++){
@@ -87,7 +145,7 @@ class Editor {
         if(e){
             if(this.matrix[i][j] == 0){
                 this.matrix[i][j] = 1;
-                e.className = 'filled-square'
+                e.className = 'square'
                 e.style.backgroundColor = this.color;
             }else{
                 this.matrix[i][j] = 0;
@@ -115,7 +173,7 @@ class Editor {
     coloringEditor(){
         const e = document.getElementById("itemColor");
         this.color = e.value;
-        element = document.getElementsByClassName('filled-square');
+        element = document.getElementsByClassName('square');
         for(let i = 0; i < element.length; i++){
             element[i].style.backgroundColor = this.color;
         }
@@ -125,6 +183,7 @@ class Editor {
             const objects = localStorage.getItem('objects') ? JSON.parse(localStorage.getItem('objects')) : [];
             objects.push({color: this.color, matrix: this.matrix});
             localStorage.setItem('objects', JSON.stringify(objects));
+            this.printExistingBlocks();
             alert('Ez az elem felkerült a listára!');
         }else{
             alert('Ez az elem már létezik!');
